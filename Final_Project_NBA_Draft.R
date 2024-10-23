29072470bd0a01acd37dc45264a22e4e6f10b8d4
joshcordek <josh_cordek1@baylor.edu>
2024-10-23 08:49:00 -0500

    Update Final_Project_NBA_Draft.R

--- Final_Project_NBA_Draft.R
+++ Final_Project_NBA_Draft.R
@@ -1,3 +1,5 @@
 library(tidyverse)
-uncleaned_df <- read_csv("nbaplayersdraft.csv")
-cleaned_df <- na.omit(uncleaned_df)
+nba_uncleaned_df <- read_csv("nbaplayersdraft.csv")
+nba_cleaned_df <- nba_uncleaned_df %>%
+  filter(!if_any(-college, is.na))
+# Only need 4 main findings
